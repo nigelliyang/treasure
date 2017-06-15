5ce7f6b895b80a628f8e9fd59cce800deae1556b
chenning0115 <chenning0115@163.com>
2017-06-15 11:36:22 +0800

    new env

--- a3c/environment.py
+++ a3c/environment.py
@@ -63,3 +63,98 @@ class futuresGame:
         info = {}
         self.totalReward += reward
         return [observation, self.mAssetAllocation, percentageReward, done, info]
+
+
+class FuturesGame_cn(object):
+
+    def __init__(self,data,step = 30):
+        self._data = data
+        self._step = step
+
+        self.support_empty = 1 #mush be 1 or 0
+        self.future_num = data.future_num
+        self.info_field_num = data.info_field_num
+
+        self.day_sign = None
+        self.price = None #shape=(minutes_num,asset_num)
+        self.daydata = None #shape=(minutes_num,asset_num,info_field_num)
+        self.minutes_num = None
+
+        self.total_property = None
+        self.allocation = None
+        self.time = None #current time
+        self.terminate = True
+
+
+    def reset(self, init_property = 50000):
+        self.total_property = init_property
+        # random extract ont day dato as an episode
+        self.day_sign,self.price,self.daydata = self._data.extract_day()
+        self.minutes_num = self.price.shape[0]
+        # current time
+        # the state is before current time open price
+        # 30 min state is 0 - 29 minutes open, close, etc. we buy futures condition on the history info
+        self.time = self._step
+        self.terminate = False
+        self.allocation = np.zeros(shape=(self.future_num + self.support_empty))
+        if self.support_empty:
+            self.allocation[-1] = 1
+
+        return self.daydata[self.time - self._step:self.time].reshape((self._step,self.future_num * self.info_field_num))
+
+
+    def step(self,action):
+        assert not self.terminate
+        assert len(action) == self.future_num + self.support_empty
+
+        last_price = self.price[self.time - 1] # last minute's open price
+        next_price = self.price[self.time] #at the end of this minute we make the open price as the price
+        next_allocation = np.zeros(shape=(self.future_num + self.support_empty))
+        next_property_contrib = []
+        for i_asset in range(self.future_num):
+            temp_quntity = self.total_property * action[i_asset] / last_price[i_asset] #quntity of asset
+            temp_next_property = next_price[i_asset] * temp_quntity
+            next_property_contrib.append(temp_next_property)
+        if self.support_empty == 1:
+            next_property_contrib.append(self.total_property * action[-1])
+        next_property = sum(next_property_contrib)
+        reward_percent = next_property / self.total_property
+
+        for i in range(self.future_num):
+            next_allocation[i] = next_property_contrib[i] / next_property
+        if self.support_empty == 1:
+            next_allocation[-1] = next_property_contrib[-1] / next_property
+        #update data
+        self.time += 1
+        self.total_property = next_property
+        self.allocation = next_allocation
+        next_state = self.daydata[self.time - self._step:self.time].reshape((self._step,self.future_num * self.info_field_num))
+        #check terminate
+        if self.time >= self.minutes_num:
+            self.terminate = True
+
+        return next_state,self.allocation,reward_percent,self.terminate,{}
+
+if __name__ == '__main__':
+    import futuresData as fd
+    c = fd.Futures_cn()
+    c.load_tranform(fd.path_list)
+    f = FuturesGame_cn(c,step=50)
+    f.reset()
+    for i in range(1000):
+        action = np.ones(f.future_num + f.support_empty) / (f.future_num + f.support_empty)
+        state,allo,reward,terminate,_ = f.step(action)
+        print('step=',i)
+        print('stateshape=',state.shape)
+        print('allo=',allo)
+        print('reward=',reward)
+        print('terminate=',terminate)
+
+
+
+
+
+
+
+
+
--- a3c/futuresData.py
+++ a3c/futuresData.py
@@ -60,3 +60,84 @@ class futuresData:
 
     def getPrice(self,time):
         return self.mPrice[time]
+
+
+import pandas as pd
+from collections import defaultdict
+import numpy as np
+path_list = [
+    'data/IC00.csv',
+    'data/TF.csv',
+]
+class Futures_cn(object):
+    def __init__(self):
+        self.data_df = None
+        self.future_num = 0
+        self.info_field_num = 0
+        self.days = []
+
+    def load_tranform(self,path_list,used_items = ['date','time','open','high','low','close','amount','volume']):
+        '''
+        transform the futures data.
+        :param path_list:
+        :param used_items: the used columns must contain 'date' and 'time'!
+        :return:None
+        '''
+        df_list = []
+        count = 0
+        for path in path_list:
+            temp_df = pd.read_csv(path)[used_items]
+            # temp_df.columns = [ item+'_'+str(count) for item in used_items]
+            df_list.append(temp_df)
+            count += 1
+        merge_df = df_list[0]
+        for i in range(1,len(df_list)):
+            # removed_columns.extend(['date_'+str(i),'time_'+str(i)])
+            temp_df = df_list[i]
+            merge_df = pd.merge(merge_df,temp_df,how='inner',on=['date','time'])
+
+        merge_df.sort(columns = ['date','time'])
+        self.data_df = merge_df
+        self.future_num = count
+        self.info_field_num = len(used_items) - 2
+        self.days = list(self.data_df['date'].values)
+
+
+    def extract_day(self, day = None,replace = True):
+        '''
+        extract one day info
+        :param day: given day to extract or random extract
+        :param replace: replace or not
+        :return: ndarray (minutes per day, future_num * info_field_num)
+        '''
+        if len(self.days) == 0:
+            self.days = list(self.data_df['date'].values)
+
+        choice = day if day else np.random.choice(self.days)
+        if replace:
+            self.days.remove(choice)
+        tempdata = self.data_df[self.data_df['date'] == choice].values[:,2:]
+        reshaped_data = tempdata.reshape((tempdata.shape[0],self.future_num,self.info_field_num))
+        price = reshaped_data[:,:,0]
+        return choice,price,reshaped_data
+
+
+
+if __name__ == '__main__':
+    c = Futures_cn()
+    c.load_tranform(path_list)
+    for i in range(10):
+        choice,price,reshaped_data = c.extract_day()
+        print(choice)
+        print(reshaped_data.shape)
+        print(price.shape)
+        print(price)
+        print(reshaped_data[0])
+
+
+
+
+
+
+
+
