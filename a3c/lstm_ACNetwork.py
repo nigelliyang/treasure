606afe2747848e0e4dcdead1d3ad181308fccb50
nigelboucher <nigelboucher@qq.com>
2017-11-21 17:55:34 +0800

    a3c

--- a3c/lstm_ACNetwork.py
+++ a3c/lstm_ACNetwork.py
@@ -1,4 +1,5 @@
-from network import *
+from a3c.network import *
+
 
 class LSTM_ACNetwork(BasicACNetwork):
     def __init__(self,
@@ -17,14 +18,14 @@ class LSTM_ACNetwork(BasicACNetwork):
                 self.state_init = [c_init, h_init]
                 self.c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
                 self.h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
-                state = rnn.LSTMStateTuple(self.c_in,self.h_in)
+                state = rnn.LSTMStateTuple(self.c_in, self.h_in)
                 # lstm_outputs shape [1, steps, lstm_unit]
                 # lstm_state shape [(lstm_c, lstm_h)]
                 lstm_outputs, self.lstm_state = tf.nn.dynamic_rnn(
-                      lstm_cell,
-                      inputs = lstm_in,
-                      initial_state = state,
-                      time_major = False)
+                    lstm_cell,
+                    inputs=lstm_in,
+                    initial_state=state,
+                    time_major=False)
                 # lstm_outputs shape [steps, lstm_unit]
                 lstm_outputs = tf.reshape(lstm_outputs, [-1, args.lstm_unit])
             # self.c_in = tf.placeholder(tf.float32, [1, 10])
@@ -36,7 +37,7 @@ class LSTM_ACNetwork(BasicACNetwork):
             with tf.variable_scope('Allocation_state') as vs:
                 self.allo = tf.placeholder(tf.float32, [None, self._action_size])
                 all_state = tf.concat([lstm_outputs, self.allo], axis=1)
-                W_fc0, b_fc0 = self._fc_variable([args.lstm_unit+self._action_size, args.state_feature_num])
+                W_fc0, b_fc0 = self._fc_variable([args.lstm_unit + self._action_size, args.state_feature_num])
                 self.state_feature = tf.nn.relu(tf.matmul(all_state, W_fc0) + b_fc0)
                 if args.dropout:
                     self.keep_prob = tf.placeholder(tf.float32, [])
@@ -44,7 +45,7 @@ class LSTM_ACNetwork(BasicACNetwork):
 
             with tf.variable_scope('FC_policy') as vs:
                 # the network will output the gaussian mean of size action_size-1
-                W_fc1, b_fc1 = self._fc_variable([args.state_feature_num, action_size-1])
+                W_fc1, b_fc1 = self._fc_variable([args.state_feature_num, action_size - 1])
                 # get the mean of gauss distribution
                 # self.gauss_mean has shape [steps, action_size-1]
                 self.gauss_mean = tf.matmul(self.state_feature, W_fc1) + b_fc1
@@ -54,21 +55,22 @@ class LSTM_ACNetwork(BasicACNetwork):
                 # given a, calculate the probability of a
                 self.a = tf.placeholder(tf.float32, [None, self._action_size])
                 # a_gauss_part has shape [steps, action_size-1]
-                a_gauss_part = self.a[:,:-1]
-                self.gauss_sigma = tf.placeholder("float",shape=[self._action_size-1,self._action_size-1])
-                gauss_coefficient = 1.0 / (((2.0 * np.pi) ** (self._action_size / 2.0 )) * tf.sqrt(tf.reduce_sum(tf.square(self.gauss_sigma))))
+                a_gauss_part = self.a[:, :-1]
+                self.gauss_sigma = tf.placeholder("float", shape=[self._action_size - 1, self._action_size - 1])
+                gauss_coefficient = 1.0 / (
+                ((2.0 * np.pi) ** (self._action_size / 2.0)) * tf.sqrt(tf.reduce_sum(tf.square(self.gauss_sigma))))
                 # gauss_bias shape [steps, action_size-1]
                 gauss_bias = a_gauss_part - self.gauss_mean
                 # shape [steps, action_size-1]
                 temp = tf.matmul(gauss_bias, tf.matrix_inverse(self.gauss_sigma))
                 # shape [steps]
-                temp = tf.reduce_sum(tf.multiply(temp, gauss_bias),axis=1)
+                temp = tf.reduce_sum(tf.multiply(temp, gauss_bias), axis=1)
                 # shape [steps]
-                self.pi = gauss_coefficient * tf.exp(-0.5*temp)
+                self.pi = gauss_coefficient * tf.exp(-0.5 * temp)
 
             with tf.variable_scope('FC_value') as vs:
                 W_fc2, b_fc2 = self._fc_variable([args.state_feature_num, 1])
-                v_ = tf.matmul(self.state_feature,W_fc2) + b_fc2
+                v_ = tf.matmul(self.state_feature, W_fc2) + b_fc2
                 # self.v has shape [steps, ]
                 self.v = tf.reshape(v_, [-1])
 
@@ -81,27 +83,29 @@ class LSTM_ACNetwork(BasicACNetwork):
     def run_policy_and_value(self, sess, s_t, allocation):
         # forward propagation, use the state in last step
         feed_dict = {
-            self.s : [s_t],
-            self.allo : [allocation],
-            self.c_in : self.state_value[0],
-            self.h_in : self.state_value[1]
+            self.s: [s_t],
+            self.allo: [allocation],
+            self.c_in: self.state_value[0],
+            self.h_in: self.state_value[1]
         }
         if args.dropout:
             feed_dict[self.keep_prob] = 1.0
 
-        gauss_mean_value, v_value, self.state_value = sess.run([self.gauss_mean, self.v, self.lstm_state],feed_dict = feed_dict)
+        gauss_mean_value, v_value, self.state_value = sess.run([self.gauss_mean, self.v, self.lstm_state],
+                                                               feed_dict=feed_dict)
         return (gauss_mean_value[0], v_value[0])
+
     def run_value(self, sess, s_t, allocation):
         # call this function when calculate the value of a certain state
         # this funcation won't update the state
         feed_dict = {
-            self.s : [s_t],
-            self.allo : [allocation],
-            self.c_in : self.state_value[0],
-            self.h_in : self.state_value[1]
+            self.s: [s_t],
+            self.allo: [allocation],
+            self.c_in: self.state_value[0],
+            self.h_in: self.state_value[1]
         }
         if args.dropout:
             feed_dict[self.keep_prob] = 1.0
 
-        v_value, _ = sess.run([self.v, self.lstm_state],feed_dict = feed_dict)
+        v_value, _ = sess.run([self.v, self.lstm_state], feed_dict=feed_dict)
         return v_value[0]
--- a3c/sharing_lstm_ACNetwork.py
+++ a3c/sharing_lstm_ACNetwork.py
@@ -1,5 +1,6 @@
 from a3c.network import *
 
+
 class Sharing_LSTM_ACNetwork(BasicACNetwork):
     def __init__(self,
                  action_size,
@@ -26,8 +27,8 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
                 self.lstm1_h_in = tf.placeholder(tf.float32, [args.asset_num, lstm1_cell.state_size.h])
                 # a list of tensors
                 # correspond to different asset
-                lstm1_c_in_split = tf.split(self.lstm1_c_in,args.asset_num,axis=0)
-                lstm1_h_in_split = tf.split(self.lstm1_h_in,args.asset_num,axis=0)
+                lstm1_c_in_split = tf.split(self.lstm1_c_in, args.asset_num, axis=0)
+                lstm1_h_in_split = tf.split(self.lstm1_h_in, args.asset_num, axis=0)
                 lstm1_c_split = []
                 lstm1_h_split = []
                 lstm1_output_split = []
@@ -39,14 +40,14 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
                     c_in_i = lstm1_c_in_split[iAsset]
                     h_in_i = lstm1_h_in_split[iAsset]
                     # concat c and h into a statetuple
-                    statei_tuple = rnn.LSTMStateTuple(c_in_i ,h_in_i)
+                    statei_tuple = rnn.LSTMStateTuple(c_in_i, h_in_i)
                     # lstm1_outputi in shape [1, steps, lstm1_unit]
                     # lstm1_statetuplei in shape [(lstm1_c, lstm1_h)]
                     lstm1_outputi, lstm1_statetuplei = tf.nn.dynamic_rnn(
                         lstm1_cell,
-                        inputs = lstm1_in_split[iAsset],
-                        initial_state = statei_tuple,
-                        time_major = False
+                        inputs=lstm1_in_split[iAsset],
+                        initial_state=statei_tuple,
+                        time_major=False
                     )
                     # the index of list is the num of asset
                     # lstm1_output_split shape [asset_num, lstm1_outputi]
@@ -56,11 +57,11 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
                     lstm1_output_split.append(lstm1_outputi)
                 # concat states
                 # self.lstm1_c and self.lstm1_h are operators, not value
-                self.lstm1_c = tf.concat(lstm1_c_split,0)
-                self.lstm1_h = tf.concat(lstm1_h_split,0)
+                self.lstm1_c = tf.concat(lstm1_c_split, 0)
+                self.lstm1_h = tf.concat(lstm1_h_split, 0)
 
                 # lstm1_outpust in shape [1, steps, lstm1_unit * asset_num]
-                lstm1_outputs = tf.concat(lstm1_output_split,2)
+                lstm1_outputs = tf.concat(lstm1_output_split, 2)
                 # lstm1_outputs in shape [steps, lstm1_unit * asset_num]
                 lstm1_outputs = tf.reshape(lstm1_outputs, [-1, args.lstm1_unit * args.asset_num])
 
@@ -68,7 +69,8 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
                 self.allo = tf.placeholder(tf.float32, [None, self._action_size])
                 # all_state in shape [steps, lstm1_unit * asset_num + action_size]
                 all_state = tf.concat([lstm1_outputs, self.allo], axis=1)
-                W_fc0, b_fc0 = self._fc_variable([args.lstm1_unit * args.asset_num +self._action_size, args.state_feature_num])
+                W_fc0, b_fc0 = self._fc_variable(
+                    [args.lstm1_unit * args.asset_num + self._action_size, args.state_feature_num])
                 self.state_feature = tf.nn.relu(tf.matmul(all_state, W_fc0) + b_fc0)
                 if args.dropout:
                     self.keep_prob = tf.placeholder(tf.float32, [])
@@ -76,7 +78,7 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
 
             with tf.variable_scope('FC_policy') as vs:
                 # the network will output the gaussian mean of size action_size-1
-                W_fc1, b_fc1 = self._fc_variable([args.state_feature_num, action_size-1])
+                W_fc1, b_fc1 = self._fc_variable([args.state_feature_num, action_size - 1])
                 # get the mean of gauss distribution
                 # self.gauss_mean has shape [steps, action_size-1]
                 self.gauss_mean = tf.matmul(self.state_feature, W_fc1) + b_fc1
@@ -86,21 +88,22 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
                 # given a, calculate the probability of a
                 self.a = tf.placeholder(tf.float32, [None, self._action_size])
                 # a_gauss_part has shape [steps, action_size-1]
-                a_gauss_part = self.a[:,:-1]
-                self.gauss_sigma = tf.placeholder("float",shape=[self._action_size-1,self._action_size-1])
-                gauss_coefficient = 1.0 / (((2.0 * np.pi) ** (self._action_size / 2.0 )) * tf.sqrt(tf.reduce_sum(tf.square(self.gauss_sigma))))
+                a_gauss_part = self.a[:, :-1]
+                self.gauss_sigma = tf.placeholder("float", shape=[self._action_size - 1, self._action_size - 1])
+                gauss_coefficient = 1.0 / (
+                    ((2.0 * np.pi) ** (self._action_size / 2.0)) * tf.sqrt(tf.reduce_sum(tf.square(self.gauss_sigma))))
                 # gauss_bias shape [steps, action_size-1]
                 gauss_bias = a_gauss_part - self.gauss_mean
                 # shape [steps, action_size-1]
                 temp = tf.matmul(gauss_bias, tf.matrix_inverse(self.gauss_sigma))
                 # shape [steps]
-                temp = tf.reduce_sum(tf.multiply(temp, gauss_bias),axis=1)
+                temp = tf.reduce_sum(tf.multiply(temp, gauss_bias), axis=1)
                 # shape [steps]
-                self.pi = gauss_coefficient * tf.exp(-0.5*temp)
+                self.pi = gauss_coefficient * tf.exp(-0.5 * temp)
 
             with tf.variable_scope('FC_value') as vs:
                 W_fc2, b_fc2 = self._fc_variable([args.state_feature_num, 1])
-                v_ = tf.matmul(self.state_feature,W_fc2) + b_fc2
+                v_ = tf.matmul(self.state_feature, W_fc2) + b_fc2
                 # self.v has shape [steps, ]
                 self.v = tf.reshape(v_, [-1])
 
@@ -116,8 +119,8 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
     def run_policy_and_value(self, sess, s_t, allocation):
         # forward propagation, use the state in last step
         feed_dict = {
-            self.s : [s_t],
-            self.allo : [allocation],
+            self.s: [s_t],
+            self.allo: [allocation],
             self.lstm1_c_in: self.lstm1_c_value,
             self.lstm1_h_in: self.lstm1_h_value
         }
@@ -134,14 +137,14 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
         # allocations are the most recent allocations
         # but the output of this function only depends on the last allocation
         feed_dict = {
-            self.s : s,
-            self.allo : allocations,
+            self.s: s,
+            self.allo: allocations,
             self.lstm1_c_in: self.lstm1_c_init,
             self.lstm1_h_in: self.lstm1_h_init
         }
         if args.dropout:
             feed_dict[self.keep_prob] = 1.0
-        gauss_mean_value, v_value = sess.run([self.gauss_mean, self.v],feed_dict=feed_dict)
+        gauss_mean_value, v_value = sess.run([self.gauss_mean, self.v], feed_dict=feed_dict)
         # gauss_mean_value and v_value are lists contains all steps' results, so return the lastest one
         return (gauss_mean_value[-1], v_value[-1])
 
@@ -149,10 +152,10 @@ class Sharing_LSTM_ACNetwork(BasicACNetwork):
         # when calculate the value of a certain state
         # this funcation won't update the state
         feed_dict = {
-            self.s : [s_t],
-            self.allo : [allocation],
-            self.lstm1_c_in : self.lstm1_c_value,
-            self.lstm1_h_in : self.lstm1_h_value
+            self.s: [s_t],
+            self.allo: [allocation],
+            self.lstm1_c_in: self.lstm1_c_value,
+            self.lstm1_h_in: self.lstm1_h_value
         }
         if args.dropout:
             feed_dict[self.keep_prob] = 1.0
